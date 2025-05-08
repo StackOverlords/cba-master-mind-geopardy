e01e714587a7f0095b5eb87bc7c0cea74c657eed
olivio-dev <tj.olivio.subelza.c@upds.net.bo>
2025-05-08 19:04:55 -0400

    feature: base pr

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cba-master-mind-geopardy
-web application, game, cba, development,
+## Licencia server
+Este proyecto está bajo la licencia [GNU Affero General Public License v3.0](server/LICENSE).  
+
+## Licencia frontend
+Este proyecto está bajo la licencia [GNU Affero General Public License v3.0](frontend/LICENSE).  
---
+++ server/src/config/database.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export class DBConnectorMongoose {
+    private static instance: DBConnectorMongoose;
+    private uri: string;
+
+    private constructor() {
+        this.uri = process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
+    }
+
+    public static getInstance(): DBConnectorMongoose {
+        if (!DBConnectorMongoose.instance) {
+            DBConnectorMongoose.instance = new DBConnectorMongoose();
+        }
+        return DBConnectorMongoose.instance;
+    }
+
+    public async connect(): Promise<void> {
+        try {
+            await mongoose.connect(this.uri);
+            console.log("Connected to MongoDB");
+        } catch (error) {
+            console.error("Error connecting to MongoDB", error);
+            throw error;
+        }
+    }
+}
