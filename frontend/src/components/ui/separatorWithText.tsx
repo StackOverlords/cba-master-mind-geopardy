ba4745db098cb5680149af9cc2a21c15e720a8ad
DESKTOP-2J1P6GT <ronaldgallardoarce78@gmail.com>
2025-05-13 17:41:26 -0400

    feat(auth): implement login functionality & firebase integration

---
+++ frontend/src/components/ui/separatorWithText.tsx
@@ -0,0 +1,16 @@
+interface Props {
+    text?: string;
+    className?: string;
+}
+
+const SeparatorWithText: React.FC<Props> = ({ text, className }) => {
+    return (
+        <div className={`flex items-center justify-center gap-3 mt-4 ${className}`}>
+            <hr className="flex-grow border-t border-gray-500" />
+            <span className="text-sm text-gray-500 font-medium">{text}</span>
+            <hr className="flex-grow border-t border-gray-500" />
+        </div>
+    );
+};
+export default SeparatorWithText;
+
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import '@fontsource-variable/onest/index.css';
+import { apiVariables } from './config/env.ts';
+import axios from 'axios';
+import { checkAuthOnStart } from './services/authService.ts';
 
+checkAuthOnStart()
+axios.defaults.baseURL = apiVariables.apiUrl
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
