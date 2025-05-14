8c0ef20599860ec6fbdee5f4ab9034ae519c5586
DESKTOP-2J1P6GT <ronaldgallardoarce78@gmail.com>
2025-05-14 18:19:34 -0400

    feat(auth): implement sign up functionality & firebase integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,18 @@ import { Route, Routes } from "react-router";
 import MainLayout from "./layouts/mainLayout";
 import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
+import LoginForm from "./components/auth/loginForm";
+import SignUpForm from "./components/auth/registerForm";
 
 const App = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route index path="/" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route index path="/" element={<HomePage />} />
+        <Route element={<LoginPage />}>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+        </Route>
       </Route>
     </Routes>
   );
--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,9 +1,20 @@
-import LoginForm from "../components/auth/loginForm";
+import { Outlet } from "react-router";
+import logoMastermindDark from "../assets/appLogo/Logo-MasterMind.webp"
 
 const LoginPage = () => {
-    return ( 
-        <LoginForm/>
-     );
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen w-full py-6 px-4">
+            <img
+                className="h-16"
+                src={logoMastermindDark} alt="Jeopardy Master Mind" />
+            <h1
+                className="text-4xl font-extrabold mb-2
+                bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">
+                Master Mind
+            </h1>
+            <Outlet />
+        </main>
+    );
 }
- 
+
 export default LoginPage;
