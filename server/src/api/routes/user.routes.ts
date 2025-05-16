4e6865ae9fbaafc5d6b84550fa0db41a851b778c
olivio-dev <tj.olivio.subelza.c@upds.net.bo>
2025-05-15 21:36:46 -0400

    feature: auth middleware/users

--- server/src/api/routes/user.routes.ts
+++ server/src/api/routes/user.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
+import { authenticateToken } from '../middlewares/auth.middleware';
 
 const router = Router();
 const userController = new UserController();
 
 // Rutas
-router.post('/create', userController.createUser.bind(userController)); // swagger true
-router.get('/firebase/:uuid', userController.getUserByFirebaseUUID.bind(userController)); // swagger true
-router.get('/', userController.getAllUsers.bind(userController)); // swagger true
-router.put('/:uuid/update', userController.updateUser.bind(userController)); // swagger true
+router.post('/create', authenticateToken, userController.createUser.bind(userController));
+router.get('/firebase/:uuid', authenticateToken, userController.getUserByFirebaseUUID.bind(userController));
+router.get('/', authenticateToken, userController.getAllUsers.bind(userController));
+router.put('/:uuid/update', authenticateToken, userController.updateUser.bind(userController));
 
-router.get('/:id', userController.getUserById.bind(userController));
-router.delete('/:uuid/delete', userController.deleteUser.bind(userController));
+router.get('/:id', authenticateToken, userController.getUserById.bind(userController));
+router.delete('/:uuid/delete', authenticateToken, userController.deleteUser.bind(userController));
 
 export default router; 
